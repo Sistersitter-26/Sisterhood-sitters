36718b74c92185024ab54a5c85113e2e517888e4
Sistersitter-26 <alisonphillip09@gmail.com>
2026-09-03 16:38:04 -0400

    Update booking email address

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Replace this with the email address that should receive booking requests.
-const BOOKING_EMAIL = "alisonphillip09@gmail.com";
+const BOOKING_EMAIL = "sisterhoodsitters3@gmail.com";
 
 const nav = document.querySelector(".nav");
 document.querySelector(".menu-btn").addEventListener("click", () => {
